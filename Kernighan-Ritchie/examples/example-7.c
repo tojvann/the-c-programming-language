5ad71eb7fbd3a03fcdaba07dc27bf39337f55965
tojvann <bravesingh2@gmail.com>
2019-12-25 13:02:22 +0530

    example 7

---
+++ Kernighan-Ritchie/examples/example-7.c
@@ -0,0 +1,27 @@
+/*
+Program to count lines, words and characters
+*/
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+int main(){
+    int c, nl, nw, nc, state;
+    nl = nw = nc = 0;
+    state = OUT;
+    while((c = getchar()) != EOF){
+        ++nc;
+        if(c == '\n')
+            ++nl;
+        if(c == ' ' || c == '\t' || c == '\n')
+            state = OUT;
+        else if(state == OUT){
+            state = IN;
+            ++nw;
+        }
+   
+    }
+    printf("%d, %d, %d\n", nl, nw, nc);
+    return 0;
+}
